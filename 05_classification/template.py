d7b9efdf4ca002b06ba7c168285cfd193675eab3
Dyri99 <dyrmundur20@ru.is>
2023-10-11 16:55:54 +0000

    gsd

--- 05_classification/template.py
+++ 05_classification/template.py
@@ -1,7 +1,13 @@
-# Author: 
-# Date:
-# Project: 
-# Acknowledgements: 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Author: Dýrmundur Helgi R. Óskarsson
+# Date: 20.9.2023
+# Project: 05 Classification
+# Acknowledgements: Einar Óskar & Torfi Tímóteus
 #
 
 
@@ -12,6 +18,11 @@ import numpy as np
 from scipy.stats import multivariate_normal
 
 
+# In[3]:
+
+
+# Section 1.1
+
 def mean_of_class(
     features: np.ndarray,
     targets: np.ndarray,
@@ -21,8 +32,21 @@ def mean_of_class(
     Estimate the mean of a selected class given all features
     and targets in a dataset
     '''
-    ...
+    class_mean = np.mean(features[targets == selected_class], axis=0)
+    
+    return class_mean
+   
 
+#features, targets, classes = load_iris()
+#(train_features, train_targets), (test_features, test_targets) = split_train_test(features, targets, train_ratio = 0.6)
+
+#print(mean_of_class(train_features, train_targets, 0))
+
+
+# In[4]:
+
+
+# Section 1.2
 
 def covar_of_class(
     features: np.ndarray,
@@ -33,8 +57,17 @@ def covar_of_class(
     Estimate the covariance of a selected class given all
     features and targets in a dataset
     '''
-    ...
+    class_cov = np.cov(features[targets == selected_class], rowvar=False)
+    
+    return class_cov
 
+#print(covar_of_class(train_features, train_targets, 0))
+
+
+# In[5]:
+
+
+# Section 1.3
 
 def likelihood_of_class(
     feature: np.ndarray,
@@ -46,7 +79,17 @@ def likelihood_of_class(
     from a multivariate normal distribution, given the mean
     and covariance of the distribution.
     '''
-    ...
+    mvn = multivariate_normal(class_mean, class_covar)
+    likelihood = mvn.pdf(feature)
+    
+    return likelihood
+    
+#class_mean = mean_of_class(train_features, train_targets, 0)
+#class_cov = covar_of_class(train_features, train_targets, 0)
+#print(likelihood_of_class(test_features[0, :], class_mean, class_cov))
+
+
+# In[6]:
 
 
 def maximum_likelihood(
@@ -66,12 +109,31 @@ def maximum_likelihood(
     '''
     means, covs = [], []
     for class_label in classes:
-        ...
+        class_mean = mean_of_class(train_features, train_targets, class_label)
+        class_cov = covar_of_class(train_features, train_targets, class_label)
+        
+        means.append(class_mean), covs.append(class_cov)
     likelihoods = []
     for i in range(test_features.shape[0]):
-        ...
+        likelihood = []
+        for j in range(len(classes)):
+            likelihood.append(likelihood_of_class(train_features[i], means[j], covs[j]))
+        likelihoods.append(likelihood)
     return np.array(likelihoods)
 
+#print(maximum_likelihood(train_features, train_targets, test_features, classes))
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+# Section 1.5
 
 def predict(likelihoods: np.ndarray):
     '''
@@ -82,8 +144,16 @@ def predict(likelihoods: np.ndarray):
     You should return a [likelihoods.shape[0]] shaped numpy
     array of predictions, e.g. [0, 1, 0, ..., 1, 2]
     '''
-    ...
+    return np.argmax(likelihoods, axis=1)
 
+#likelihoods = maximum_likelihood(train_features, train_targets, test_features, classes)
+#print(predict(likelihoods))
+
+
+# In[10]:
+
+
+# Section 2.1
 
 def maximum_aposteriori(
     train_features: np.ndarray,
@@ -100,4 +170,57 @@ def maximum_aposteriori(
     a [test_features.shape[0] x len(classes)] shaped numpy
     array
     '''
-    ...
+    means, covs, priors = [], [], []
+    length_features = len(train_features)
+    for class_label in classes:
+        class_mean = mean_of_class(train_features, train_targets, class_label)
+        class_cov = covar_of_class(train_features, train_targets, class_label)
+        prior = len(train_features[train_targets == class_label]) / length_features
+        
+        means.append(class_mean), covs.append(class_cov), priors.append(prior)
+    likelihoods = []
+    for i in range(test_features.shape[0]):
+        likelihood = []
+        for j in range(len(classes)):
+            likelihood.append((likelihood_of_class(train_features[i], means[j], covs[j])) * priors[j])
+        likelihoods.append(likelihood)
+    return np.array(likelihoods)
+
+#posteriors = maximum_aposteriori(train_features, train_targets, test_features, classes)
+#print(posteriors)
+
+#predictions = predict(posteriors)
+#print(predictions)
+
+
+# In[31]:
+
+
+# Section 2.2
+
+#predictions = predict(likelihoods)
+#predictions = predict(posteriors)
+
+#print(train_targets[0:len(predictions)])
+#count=0
+#for i, tt in enumerate(train_targets[0:len(predictions)]):
+#    if tt==predictions[i]:
+#        count+=1
+#print(count/len(predictions))
+
+#length = len(classes)
+#confusion_matrix = np.zeros((length, length), dtype='int')
+
+#for i in range(len(predictions)):
+#    correct = train_targets[i]
+#    guess = predictions[i]
+#    confusion_matrix[guess][correct] += 1
+    
+#print(confusion_matrix)
+
+
+# In[ ]:
+
+
+
+
--- 06_PCA/template.py
+++ 06_PCA/template.py
@@ -1,7 +1,13 @@
-# Author: 
-# Date:
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Author: Dýrmundur Helgi
+# Date: 26.09.2023
 # Project: 
-# Acknowledgements: 
+# Acknowledgements: Einar & Torfi
 #
 
 # NOTE: Your code should NOT contain any main functions or code that is executed
@@ -17,6 +23,11 @@ from sklearn.decomposition import PCA
 from tools import load_cancer
 
 
+# In[2]:
+
+
+# Section 1.1
+
 def standardize(X: np.ndarray) -> np.ndarray:
     '''
     Standardize an array of shape [N x 1]
@@ -28,8 +39,17 @@ def standardize(X: np.ndarray) -> np.ndarray:
     (np.ndarray): A standardized version of X, also
     of shape [N x 1]
     '''
-    ...
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return (X - mean) / std
 
+#print(standardize(np.array([[0, 0], [0, 0], [1, 1], [1, 1]])))
+
+
+# In[3]:
+
+
+# Section 1.2
 
 def scatter_standardized_dims(
     X: np.ndarray,
@@ -45,42 +65,137 @@ def scatter_standardized_dims(
     * i (int): The first index
     * j (int): The second index
     '''
-    ...
+    standardized_X = standardize(X)
+    
+    x_dim = standardized_X[:, i]
+    y_dim = standardized_X[:, j]
+    
+    plt.scatter(x_dim, y_dim)
+    plt.xlabel(f'Dimension {i}')
+    plt.ylabel(f'Dimension {j}')
+    #plt.title('Scatter plot of standardized dimensions')
+    
+#X = np.array([
+#    [1, 2, 3, 4],
+#    [0, 0, 0, 0],
+#    [4, 5, 5, 4],
+#    [2, 2, 2, 2],
+#    [8, 6, 4, 2]])
+#scatter_standardized_dims(X, 0, 2)
 
+
+# In[4]:
+
+
+# Section 1.3
 
 def _scatter_cancer():
     X, y = load_cancer()
-    ...
+    standardized_X = standardize(X[:5])
+    plt.figure(figsize=(15, 10))
+    for i in range(X.shape[1]):
+        plt.subplot(5,6,i+1)
+        scatter_standardized_dims(X[:5], 0, i)
+    plt.tight_layout()
+    
+#_scatter_cancer()
 
+
+# In[16]:
+
+
+# Section 2.1
 
 def _plot_pca_components():
-    ...
     X, y = load_cancer()
-    for i in range(...):
-        plt.subplot(5, 6, ...)
-        ...
-    plt.show()
+    standardized_X = standardize(X)
+    pca = PCA(n_components = X.shape[1])
+    principal_components = pca.fit_transform(standardized_X)
+    plt.figure(figsize=(20,16))
+    for i in range(3):
+        plt.subplot(5, 6, i+1)
+        plt.plot(range(len(principal_components)), principal_components[:, i])
+        plt.title(f'Component {i}')
 
+
+#_plot_pca_components()
+
+
+# In[24]:
+
+
+# Section 3.1
 
 def _plot_eigen_values():
-    ...
+    X, y = load_cancer()
+    standardized_X = standardize(X)
+    
+    pca = PCA(n_components = min(X.shape))
+    pca.fit(standardized_X)
+    
+    eigen_values = pca.explained_variance_
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(eigen_values) + 1), eigen_values, marker='o')
     plt.xlabel('Eigenvalue index')
     plt.ylabel('Eigenvalue')
     plt.grid()
     plt.show()
 
+#_plot_eigen_values()
+
+
+# In[25]:
+
+
+# Section 3.2
 
 def _plot_log_eigen_values():
-    ...
+    X, y = load_cancer()
+    standardized_X = standardize(X)
+    
+    pca = PCA(n_components = min(X.shape))
+    pca.fit(standardized_X)
+    
+    eigen_values = pca.explained_variance_
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(eigen_values) + 1), np.log10(eigen_values), marker='o')
     plt.xlabel('Eigenvalue index')
     plt.ylabel('$\log_{10}$ Eigenvalue')
     plt.grid()
     plt.show()
+    
+#_plot_log_eigen_values()
 
+
+# In[26]:
+
+
+# Section 3.3
 
 def _plot_cum_variance():
-    ...
+    X, y = load_cancer()
+    standardized_X = standardize(X)
+    
+    pca = PCA(n_components = min(X.shape))
+    pca.fit(standardized_X)
+    
+    eigen_values = pca.explained_variance_
+    cumulative_variance = np.cumsum(eigen_values) / np.sum(eigen_values)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(range(1, len(cumulative_variance) + 1), cumulative_variance, marker='o')
     plt.xlabel('Eigenvalue index')
     plt.ylabel('Percentage variance')
     plt.grid()
     plt.show()
+    
+#_plot_cum_variance()
+
+
+# In[ ]:
+
+
+
+
