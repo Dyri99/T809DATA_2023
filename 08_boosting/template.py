2e18c73b0440e149e9d2373dddf4adcd3f19fb6a
Dyri99 <dyrmundur20@ru.is>
2023-10-24 20:23:10 +0000

    g

--- 08_boosting/template.py
+++ 08_boosting/template.py
@@ -1,6 +1,12 @@
-# Author: 
-# Date:
-# Project: 
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Author: Dýrmundur Helgi R. Óskarsson
+# Date: 12.10.2023
+# Project: 08 Boosting
 # Acknowledgements: 
 #
 
@@ -23,29 +29,129 @@ from sklearn.metrics import (confusion_matrix, accuracy_score, recall_score, pre
 from tools import get_titanic, build_kaggle_submission
 
 
+# In[2]:
+
+
+# Section 1.1
+
 def get_better_titanic():
     '''
     Loads the cleaned titanic dataset but change
     how we handle the age column.
     '''
-    pass
+    # Load in the raw data
+    # check if data directory exists for Mimir submissions
+    # DO NOT REMOVE
+    if os.path.exists('./data/train.csv'):
+        train = pd.read_csv('./data/train.csv')
+        test = pd.read_csv('./data/test.csv')
+    else:
+        train = pd.read_csv('train.csv')
+        test = pd.read_csv('test.csv')
 
+    # Concatenate the train and test set into a single dataframe
+    # we drop the `Survived` column from the train set
+    X_full = pd.concat([train.drop('Survived', axis=1), test], axis=0)
+    
+    median_age = X_full['Age'].median()
+    X_full['Age'].fillna(median_age, inplace=True)
+
+    # The cabin category consist of a letter and a number.
+    # We can divide the cabin category by extracting the first
+    # letter and use that to create a new category. So before we
+    # drop the `Cabin` column we extract these values
+    X_full['Cabin_mapped'] = X_full['Cabin'].astype(str).str[0]
+    # Then we transform the letters into numbers
+    cabin_dict = {k: i for i, k in enumerate(X_full.Cabin_mapped.unique())}
+    X_full.loc[:, 'Cabin_mapped'] =        X_full.loc[:, 'Cabin_mapped'].map(cabin_dict)
+
+    # We drop multiple columns that contain a lot of NaN values
+    # in this assignment
+    # Maybe we should
+    X_full.drop(
+        ['PassengerId', 'Cabin', 'Name', 'Ticket'],
+        inplace=True, axis=1)
+
+    # Instead of dropping the fare column we replace NaN values
+    # with the 3rd class passenger fare mean.
+    fare_mean = X_full[X_full.Pclass == 3].Fare.mean()
+    X_full['Fare'].fillna(fare_mean, inplace=True)
+    # Instead of dropping the Embarked column we replace NaN values
+    # with `S` denoting Southampton, the most common embarking
+    # location
+    X_full['Embarked'].fillna('S', inplace=True)
+
+    # We then use the get_dummies function to transform text
+    # and non-numerical values into binary categories.
+    X_dummies = pd.get_dummies(
+        X_full,
+        columns=['Sex', 'Cabin_mapped', 'Embarked'],
+        drop_first=True)
+
+    # We now have the cleaned data we can use in the assignment
+    X = X_dummies[:len(train)]
+    submission_X = X_dummies[len(train):]
+    y = train.Survived
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=.3, random_state=5, stratify=y)
+
+    return (X_train, y_train), (X_test, y_test), submission_X
+
+#get_better_titanic()
+
+
+# In[3]:
+
+
+# Section 2.1
 
 def rfc_train_test(X_train, t_train, X_test, t_test):
     '''
     Train a random forest classifier on (X_train, t_train)
     and evaluate it on (X_test, t_test)
     '''
-    pass
+    rfc = RandomForestClassifier()
+    rfc.fit(X_train, t_train)
+    predictions = rfc.predict(X_test)
+    
+    accuracy = accuracy_score(t_test, predictions)
+    precision = precision_score(t_test, predictions)
+    recall = recall_score(t_test, predictions)
+    
+    return accuracy, precision, recall
 
+#(tr_X, tr_y), (tst_X, tst_y), _ = get_titanic()
+#rfc_train_test(tr_X, tr_y, tst_X, tst_y)
+
+
+# In[10]:
+
+
+# Section 2.3
 
 def gb_train_test(X_train, t_train, X_test, t_test):
     '''
     Train a Gradient boosting classifier on (X_train, t_train)
     and evaluate it on (X_test, t_test)
     '''
-    pass
+    gbc = GradientBoostingClassifier()
+    gbc.fit(X_train, t_train)
+    predictions = gbc.predict(X_test)
+    
+    accuracy = accuracy_score(t_test, predictions)
+    precision = precision_score(t_test, predictions)
+    recall = recall_score(t_test, predictions)
+    
+    return accuracy, precision, recall
 
+#(tr_X, tr_y), (tst_X, tst_y), _ = get_titanic()
+#gb_train_test(tr_X, tr_y, tst_X, tst_y)
+
+
+# In[23]:
+
+
+# Section 2.5
 
 def param_search(X, y):
     '''
@@ -54,9 +160,9 @@ def param_search(X, y):
     '''
     # Create the parameter grid
     gb_param_grid = {
-        'n_estimators': [...],
-        'max_depth': [...],
-        'learning_rate': [...]}
+        'n_estimators': [i for i in range(1, 101)],
+        'max_depth': [i for i in range(1, 51)],
+        'learning_rate': [i for i in np.linspace(1,1,200)]}
     # Instantiate the regressor
     gb = GradientBoostingClassifier()
     # Perform random search
@@ -72,6 +178,15 @@ def param_search(X, y):
     # Print the best parameters and lowest RMSE
     return gb_random.best_params_
 
+#(tr_X, tr_y), (tst_X, tst_y), _ = get_titanic()
+#best_params = param_search(tr_X, tr_y)
+#print(best_params)
+
+
+# In[24]:
+
+
+# Section 2.6
 
 def gb_optimized_train_test(X_train, t_train, X_test, t_test):
     '''
@@ -79,12 +194,29 @@ def gb_optimized_train_test(X_train, t_train, X_test, t_test):
     and evaluate it on (X_test, t_test) with
     your own optimized parameters
     '''
-    pass
+    best_params = param_search(X_train, t_train)
+    
+    gb_optimized = GradientBoostingClassifier(
+        n_estimators = best_params['n_estimators'],
+        max_depth = best_params['max_depth'],
+        learning_rate = best_params['learning_rate']
+    )
+    gb_optimized.fit(X_train, t_train)
+    
+    t_pred = gb_optimized.predict(X_test)
+    
+    accuracy = accuracy_score(t_test, t_pred)
+    precision = precision_score(t_test, t_pred)
+    recall = recall_score(t_test, t_pred)
+    
+    return accuracy, precision, recall
+
+#(tr_X, tr_y), (tst_X, tst_y), _ = get_titanic()
+#gb_optimized_train_test(tr_X, tr_y, tst_X, tst_y)
 
 
-def _create_submission():
-    '''Create your kaggle submission
-    '''
-    pass
-    prediction = None # !!! Your prediction here !!!
-    build_kaggle_submission(prediction)
+# In[ ]:
+
+
+
+
